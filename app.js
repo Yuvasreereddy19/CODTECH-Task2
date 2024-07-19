8f8fe45e7e4fca563fe74ebfe3cb36ab1b86a47e
Yuvasreereddy19 <yuvasreereddyjanga@gmail.com>
2024-07-19 23:30:02 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,15 @@
+// src/App.js
+import React from 'react';
+import Header from './components/header';
+import HomePage from './pages/HomePage';
+
+const App = () => {
+  return (
+    <div className="App">
+      <Header />
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
